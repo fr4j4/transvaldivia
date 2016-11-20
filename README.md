470791b963d8e6c00b11e8824644c93dafd9df58
Francisco <fgonz@outlook.com>
2016-11-20 02:27:41 -0300

    configuracion de mysql

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # transValdivia
+requerimientos
+
+- python pip
+
+- mysqlclient
+	sudo apt-get install libmysqlclient-dev
+- mysql connector
+	https://dev.mysql.com/downloads/file/?id=465651
--- transvaldivia/settings.py
+++ transvaldivia/settings.py
@@ -72,13 +72,16 @@ WSGI_APPLICATION = 'transvaldivia.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+#Configurado para usar mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'my.cnf',
+        },
     }
 }
+
 
 
 # Password validation
