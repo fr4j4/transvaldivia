764fe089d828e95511fb168f2dba628271c3a0da
Francisco <fgonz@outlook.com>
2016-12-18 15:13:50 -0300

    template principal

--- principal/views.py
+++ principal/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from principal.models import *
+from django.template import RequestContext,loader
 # Create your views here.
 def indice(request):
 	return HttpResponse("Hola mundo. Este es el indice")
 
+
+
 def principal_index(request):
-	return HttpResponse("Hola mundo. Este es el indice de principal")
+	template=loader.get_template('principal/index.html')  
+	context=RequestContext(request)
+	return HttpResponse(template.render(context))
+	#return HttpResponse("Hola mundo. Este es el indice de principal")
