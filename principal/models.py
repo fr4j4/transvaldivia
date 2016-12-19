06810c8a509be62dc99eb70b495802eba9d5ebd5
Alejandra <ale_hg13@hotmail.com>
2016-12-18 22:21:35 -0300

    migraciones

--- principal/models.py
+++ principal/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 # Create your models here.
-class linea(models.Model):
-	nombre = models.CharField(max_length=200)
-	color=models.CharField(max_length=20)
-
-class recorrido(models.Model):
-	nombre= models.CharField(max_length=200)
-	mapa=models.CharField(max_length=200)
-	horario_inicio=models.TimeField(auto_now=False, auto_now_add=False)
-	horario_final=models.TimeField(auto_now=False, auto_now_add=False)
-
 class tipo_vehiculo(models.Model):
 	nombre=models.CharField(max_length=200)
 	class Meta:
@@ -21,6 +11,30 @@ class tipo_vehiculo(models.Model):
 class paradero(models.Model):
 	nombre=models.CharField(max_length=200)
 	latitud=models.FloatField()
-	longitud=models.FloatField()
+	longitud=models.FloatField()	
+
+class recorrido(models.Model):
+	nombre= models.CharField(max_length=200)
+	mapa=models.CharField(max_length=200)
+	horario_inicio=models.TimeField(auto_now=False, auto_now_add=False)
+	horario_final=models.TimeField(auto_now=False, auto_now_add=False)
+	#paradero=models.ManyToManyField(paradero)
+
+class linea(models.Model):
+	nombre = models.CharField(max_length=200)
+	color=models.CharField(max_length=20)
+	tipo_vehiculo=models.ForeignKey(tipo_vehiculo,null=False)
+	recorrido = models.ManyToManyField(recorrido)
+
+
+class recorrido_paradero(models.Model):
+	recorrido=models.ForeignKey(recorrido, on_delete=models.CASCADE)
+	paradero=models.ForeignKey(paradero, on_delete=models.CASCADE)
+	pos= models.IntegerField()
+
+
+
+
+
 
 
